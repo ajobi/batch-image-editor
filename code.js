f2eff7fe232ae80aa79b764aee2fa5b9804b9771
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-12-06 13:48:58 +0100

    :package: New: Basic markup for the filter controls added

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// figma.showUI(__html__)
+figma.showUI(__html__)
 
 for (const node of figma.currentPage.selection) {
   const fill = node.fills[0]
@@ -6,12 +6,14 @@ for (const node of figma.currentPage.selection) {
   if (fill && fill.type === 'IMAGE') {
     const newFill = JSON.parse(JSON.stringify(fill))
 
-    newFill.filters.exposure = 0.15
-    newFill.filters.contrast = 0.45
-    newFill.filters.temperature = -0.25
+    console.log(newFill)
 
-    node.fills = [newFill]
+    // newFill.filters.exposure = 0.15
+    // newFill.filters.contrast = 0.45
+    // newFill.filters.temperature = -0.25
+
+    // node.fills = [newFill]
   }
 }
 
-figma.closePlugin()
+// figma.closePlugin()
