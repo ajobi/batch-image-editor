eea2bef72c7973993e22c0d2a4579f453fa8cfa8
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-12-06 12:02:30 +0100

    :package: New: Hardcoded image fill manipulation added

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-figma.showUI(__html__)
+// figma.showUI(__html__)
 
-// figma.closePlugin()
+for (const node of figma.currentPage.selection) {
+  const fill = node.fills[0]
+
+  if (fill && fill.type === 'IMAGE') {
+    const newFill = JSON.parse(JSON.stringify(fill))
+
+    newFill.filters.exposure = 0.15
+    newFill.filters.contrast = 0.45
+    newFill.filters.temperature = -0.25
+
+    node.fills = [newFill]
+  }
+}
+
+figma.closePlugin()
