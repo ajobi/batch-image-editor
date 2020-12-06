0bfadd613c68be58ed0155b4cd359dda56f70eed
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-12-06 14:15:04 +0100

    :package: New: Applying of input filters added

--- code.js
+++ code.js
@@ -1,19 +1,13 @@
 figma.showUI(__html__)
 
-for (const node of figma.currentPage.selection) {
-  const fill = node.fills[0]
+figma.ui.onmessage = (filterValues) => {
+  for (const node of figma.currentPage.selection) {
+    const fill = node.fills[0]
 
-  if (fill && fill.type === 'IMAGE') {
-    const newFill = JSON.parse(JSON.stringify(fill))
-
-    console.log(newFill)
-
-    // newFill.filters.exposure = 0.15
-    // newFill.filters.contrast = 0.45
-    // newFill.filters.temperature = -0.25
-
-    // node.fills = [newFill]
+    if (fill && fill.type === 'IMAGE') {
+      const newFill = JSON.parse(JSON.stringify(fill))
+      newFill.filters = filterValues
+      node.fills = [newFill]
+    }
   }
 }
-
-// figma.closePlugin()
