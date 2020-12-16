945be3c703e6cb23af692a3a62635c4b1a706b16
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-12-16 17:24:53 +0100

    :package: New: Filters applied to the newly selected layers

--- code.js
+++ code.js
@@ -1,13 +1,22 @@
 figma.showUI(__html__, { width: 300, height: 250 })
 
-figma.ui.onmessage = (filterValues) => {
+let pluginFilters = null
+
+const applyFilters = () => {
   for (const node of figma.currentPage.selection) {
     const fill = node.fills ? node.fills[0] : null
 
     if (fill && fill.type === 'IMAGE') {
       const newFill = JSON.parse(JSON.stringify(fill))
-      newFill.filters = filterValues
+      newFill.filters = pluginFilters
       node.fills = [newFill]
     }
   }
 }
+
+figma.ui.onmessage = filterValues => {
+  pluginFilters = filterValues
+  applyFilters()
+}
+
+figma.on("selectionchange", applyFilters)
