9e9c428f1d70edaf36ad90d74a688de53b1d9b5b
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-12-17 16:21:42 +0100

    :package: New: html webpack plugin setup added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
 
 module.exports = (env, argv) => ({
   mode: 'production',
@@ -7,5 +8,11 @@ module.exports = (env, argv) => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'code.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+    }),
+  ]
 })
